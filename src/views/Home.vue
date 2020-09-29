1faeac735d6a70806537443441155f1ba9654513
Leo Champion <leo.champion@protonmail.com>
2020-09-30 00:24:31 +0200

    remove HelloWord.vue component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld />
+    <div class="hello">
+      <h1>AOE 2 hotkey tool</h1>
+      <p>
+        Improve your knowledge and skill of your Age Of Empire II hotkeys.
+      </p>
+    </div>
   </div>
 </template>
 
